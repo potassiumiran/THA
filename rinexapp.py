06cd27f16ef1a7d6fde42e46d13d33e31d0d577d
potassiumiran <87702327+potassiumiran@users.noreply.github.com>
2021-07-20 22:06:58 +0530

    Create rinexapp.py

---
+++ rinexapp.py
@@ -0,0 +1,140 @@
+from telegram.ext import Updater,MessageHandler,Filters
+
+
+def lighton(bot,update):
+  chat_id=bot.message.chat_id
+  path='https://cdn.pixabay.com/photo/2019/09/29/22/06/light-bulb-4514505_1280.jpg'
+  update.bot.sendPhoto(chat_id=chat_id,photo=path)
+  bot.message.reply_text("the light is on")
+  val=1
+  from Adafruit_IO import Client
+  aio = Client('kirran', 'aio_rNfG2347nDd0C7faT21cKTK5o6Px')
+
+
+  aio.send('light', val)
+
+
+  data = aio.receive('light')
+  print('Received value: {0}'.format(data.value))
+  
+
+def lightoff(bot,update):
+  chat_id=bot.message.chat_id
+  path='https://cdn5.vectorstock.com/i/1000x1000/70/44/3d-realistic-off-light-bulb-icon-closeup-vector-27407044.jpg'
+  update.bot.sendPhoto(chat_id=chat_id,photo=path)
+  bot.message.reply_text("the light is off")
+  val=0
+  from Adafruit_IO import Client
+  aio = Client('kirran', 'aio_rNfG2347nDd0C7faT21cKTK5o6Px')
+
+
+  aio.send('light', val)
+
+
+  data = aio.receive('light')
+  print('Received value: {0}'.format(data.value))
+
+def fanon(bot,update):
+  chat_id=bot.message.chat_id
+  path='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/decorative-ceiling-fan-in-motion-b-john-myers.jpg'
+  update.bot.sendPhoto(chat_id=chat_id,photo=path)
+  bot.message.reply_text("the fan is on")
+  val=1
+  from Adafruit_IO import Client
+  aio = Client('kirran', 'aio_rNfG2347nDd0C7faT21cKTK5o6Px')
+
+
+  aio.send('fan', val)
+
+
+  data = aio.receive('fan')
+  print('Received value: {0}'.format(data.value))
+
+
+def fanoff(bot,update):
+  chat_id=bot.message.chat_id
+  path='https://5.imimg.com/data5/PN/HR/MY-8054528/pooja-meghdoot-high-speed-ceiling-fan-500x500.jpg'
+  update.bot.sendPhoto(chat_id=chat_id,photo=path)
+  bot.message.reply_text("the fan is off")
+  val=0
+  from Adafruit_IO import Client
+  aio = Client('kirran', 'aio_rNfG2347nDd0C7faT21cKTK5o6Px')
+
+
+  aio.send('fan', val)
+
+
+  data = aio.receive('fan')
+  print('Received value: {0}'.format(data.value))
+
+
+
+
+def accept(bot,update):
+
+  mark=False
+
+  a=bot.message.text.lower()
+
+
+  b=a.split()
+
+  for i in b:
+    if i=="light":
+      for j in b:
+        if j=="on":
+          lighton(bot,update)
+        elif j=="off":
+          lightoff(bot,update)
+  
+    
+
+
+  for i in b:
+
+    if i=="fan":
+      for j in b:
+        if j=="on":
+          fanon(bot,update)
+        elif j=="off":
+          fanoff(bot,update)
+
+  for i in b:
+    if i == "light" or i == "fan":
+      mark=True
+      break
+
+  if mark==False:
+     bot.message.reply_text("invalid command, specify the device and function to be performed together")
+  elif mark==True:
+    mark=False
+
+  
+      
+
+     
+    
+
+
+ 
+
+  
+
+
+
+
+  
+
+
+
+
+
+
+
+BOT_TOKEN='1924859836:AAFTOIeoH_Twbc2KjeWIsXh2AwxHvOcu61g'
+u=Updater(BOT_TOKEN,use_context=True)
+dp=u.dispatcher
+dp.add_handler(MessageHandler(Filters.text,accept))
+u.start_polling()
+u.idle()
+
